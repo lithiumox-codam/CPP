e15f44a02082642a432e1476582af6063e52a675
meesdekker.xyz <me@meesdekker.xyz>
2024-01-26 17:30:26 +0100

    changes megaphone

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,6 +3,10 @@
 #include <string>
 
 int main(int ac, char **av) {
+  if (ac == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    return 0;
+  }
   for (int i = 1; i < ac; i++) {
     std::string str = av[i];
     for (size_t j = 0; j < str.length(); j++) {
