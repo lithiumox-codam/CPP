b110eea597e9c814779f8679b9d3c8bb2a6033dd
meesdekker.xyz <me@meesdekker.xyz>
2024-03-27 18:29:42 +0100

    small fixes

--- 02/ex01/Fixed.cpp
+++ 02/ex01/Fixed.cpp
@@ -1,5 +1,6 @@
 #include "Fixed.hpp"
 
+#include <cmath>
 #include <iostream>
 
 Fixed::Fixed() : _value(0) { std::cout << "Default constructor called" << std::endl; }
@@ -16,7 +17,7 @@ Fixed::Fixed(const int value) {
 
 Fixed::Fixed(const float value) {
 	std::cout << "Float constructor called" << std::endl;
-	_value = static_cast<int>(roundf(value * (1 << _fractionalBits)));
+	_value = static_cast<int>(std::roundf(value * (1 << _fractionalBits)));
 }
 
 Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
