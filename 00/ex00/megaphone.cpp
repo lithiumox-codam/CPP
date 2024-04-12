8782bf2de2623376d49f363ff74f933089a2e31e
meesdekker.xyz <me@meesdekker.xyz>
2024-04-12 13:50:11 +0200

    Small changes

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -10,7 +10,7 @@
  */
 int main(int ac, char **av) {
 	if (ac == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	} else {
 		for (int i = 1; i < ac; i++) {
