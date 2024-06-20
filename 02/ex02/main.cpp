bd840d486159625e03900c5c1f8c66c14ae4119c
meesdekker.xyz <me@meesdekker.xyz>
2024-06-20 17:05:28 +0200

    Added more tests

--- 02/ex02/main.cpp
+++ 02/ex02/main.cpp
@@ -36,6 +36,22 @@ void test_comparison(std::string name, Fixed& x, Fixed& test) {
 	if (x != test) print_comp("!=", x, test);
 }
 
+void test_min_max(std::string name, Fixed& x, Fixed& test) {
+	std::cout << "Initial value of " << name << " is " << x << std::endl;
+	std::cout << "Min of " << x << " and " << test << " is " << Fixed::min(x, test) << std::endl;
+	std::cout << "Max of " << x << " and " << test << " is " << Fixed::max(x, test) << std::endl;
+}
+
+void test_pre_post(std::string name, Fixed &x) {
+	std::cout << "Initial value of " << name << " is " << x << std::endl;
+	std::cout << "Pre-increment of " << name << " is " << ++x << std::endl;
+	std::cout << "Post-increment of " << name << " is " << x++ << std::endl;
+	std::cout << "Value of " << name << " after post-increment is " << x << std::endl;
+	std::cout << "Pre-decrement of " << name << " is " << --x << std::endl;
+	std::cout << "Post-decrement of " << name << " is " << x-- << std::endl;
+	std::cout << "Value of " << name << " after post-decrement is " << x << std::endl;
+}
+
 int main(void) {
 	const std::string names[] = {"a", "b", "c", "d"};
 	Fixed values[] = {1234.4321f, 10, 42.42f, 10};
@@ -51,6 +67,12 @@ int main(void) {
 
 	std::cout << "\nChecking comparison operators\n" << std::endl;
 	for (int i = 0; i < 4; i++) test_comparison(names[i], values[i], testValues[i]);
+
+	std::cout << "\nChecking min and max functions\n" << std::endl;
+	for (int i = 0; i < 4; i++) test_min_max(names[i], values[i], testValues[i]);
+
+	std::cout << "\nChecking pre and post increment and decrement operators\n" << std::endl;
+	for (int i = 0; i < 4; i++) test_pre_post(names[i], values[i]);
 
 	return 0;
 }
